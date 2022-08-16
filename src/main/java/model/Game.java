792e40cf378cef8fab3e3b0bca787cff49eb5768
berkergurcay <berker.gurcay@ozu.edu.tr>
2022-08-16 14:32:50 +0300

    Test if games with same home/away teams and date are equal

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -1,10 +1,16 @@
 package model;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class Game {
     private int homeScore;
     private int awayScore;
     private boolean live = true;
 
+    private String homeTeam;
+    private String awayTeam;
+    private LocalDateTime startDate;
 
     public int getHomeScore() {
         return homeScore;
@@ -30,4 +36,40 @@ public class Game {
         this.live = live;
     }
 
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public void setHomeTeam(String homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+    public void setAwayTeam(String awayTeam) {
+        this.awayTeam = awayTeam;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Game game = (Game) o;
+        return homeTeam.equals(game.homeTeam) && awayTeam.equals(game.awayTeam) && startDate.equals(game.startDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(homeTeam, awayTeam, startDate);
+    }
 }
--- src/test/java/WorldCupScoreboardTest.java
+++ src/test/java/WorldCupScoreboardTest.java
@@ -1,7 +1,9 @@
 import model.Game;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.*;
 
 public class WorldCupScoreboardTest {
 
@@ -25,5 +27,33 @@ public class WorldCupScoreboardTest {
     public void testGameLive(){
         Game game = new Game();
         assertEquals(true,game.isLive());
+    }
+
+    @Test
+    public void testGamesEqual(){
+        Game game1 = new Game();
+        game1.setHomeTeam("FC Barcelona");
+        game1.setAwayTeam("Real Madrid");
+        game1.setStartDate(LocalDateTime.of(2022,8,16,20,0,0));
+
+        Game game2 = new Game();
+        game2.setHomeTeam("FC Barcelona");
+        game2.setAwayTeam("Real Madrid");
+        game2.setStartDate(LocalDateTime.of(2022,8,16,20,0,0));
+
+
+        Game game3 = new Game();
+        game3.setHomeTeam("Real Madrid");
+        game3.setAwayTeam("FC Barcelona");
+        game3.setStartDate(LocalDateTime.of(2022,8,16,20,0,0));
+
+        Game game4 = new Game();
+        game4.setHomeTeam("Real Madrid");
+        game4.setAwayTeam("FC Barcelona");
+        game4.setStartDate(LocalDateTime.of(2021,8,16,20,0,0));
+
+        assertEquals(game1,game2);
+        assertNotEquals(game1,game3);
+        assertNotEquals(game1,game4);
     }
 }
